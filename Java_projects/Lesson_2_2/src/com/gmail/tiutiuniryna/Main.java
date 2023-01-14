900599cd1ebf2bf708c6d4df442f5b9adc223306
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-01-14 22:21:10 +0100

    Lesson 2_2

---
+++ Java_projects/Lesson_2_2/src/com/gmail/tiutiuniryna/Main.java
@@ -0,0 +1,13 @@
+package com.gmail.tiutiuniryna;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int priceApple = 2;
+		int buyApples = 6;
+		int buySum = buyApples * priceApple;
+		System.out.println("The buyer will pay for the apples : " + buySum + " $");
+
+	}
+
+}
