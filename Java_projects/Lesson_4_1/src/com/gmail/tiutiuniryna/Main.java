709ca816d80fd76fc27d1aa54495d5e0b31e52e3
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-01-26 11:25:05 +0100

    Lesson 4_1 added a check for the entered number and changed the type of the variable to store the factorial from int to long

--- Java_projects/Lesson_4_1/src/com/gmail/tiutiuniryna/Main.java
+++ Java_projects/Lesson_4_1/src/com/gmail/tiutiuniryna/Main.java
@@ -6,13 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number n");
+		System.out.println("Enter a number n (greater than 4 and less than 16)");
 		int n = sc.nextInt();
-		int factorial = 1;
-		for(int i = 1; i <= n; i++) {
-			factorial = factorial * i;
+		long factorial = 1;
+		if (n > 4 && n < 16) {
+			for (int i = 1; i <= n; i++) {
+				factorial = factorial * i;
+				
+			}
+			System.out.println("Factorial of a number " + n + "! = " + factorial);
+		} else {
+			System.out.println("Invalid number entered");
 		}
-		System.out.println("Factorial of a number " + n + "! = " + factorial);
+		
+
 		sc.close();
 
 	}
