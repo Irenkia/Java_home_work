5cd996339854cb651f31f24c98efee814e3fbae8
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-01-31 17:10:39 +0100

    Lesson 6_2

--- Java_projects/Lesson_6_2/src/com/gmail/tiutiuniryna/Main.java
+++ Java_projects/Lesson_6_2/src/com/gmail/tiutiuniryna/Main.java
@@ -7,20 +7,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input string");
+		System.out.println("Input string from words separated by spaces");
 		String str = sc.nextLine();
-		char[] strArray = str.toCharArray();
+		String[] strArray = str.split("['']");
 		System.out.println(Arrays.toString(strArray));
-		char sym = 'b';
-		int counter = 0;
+		String word = "";
 		for (int i = 0; i < strArray.length; i++) {
-			if (sym == strArray[i]) {
-				counter++;
+			if (word.length() < strArray[i].length()) {
+				word = strArray[i];
 			}
-		}
-		System.out.println("Character 'b' found in string " + counter + " times");
-		sc.close();
 
+		}
+		System.out.println("The longest word in the string - " + word);
+
+		sc.close();
 	}
 
 }
