61e2fda60e1ff5d4e2bef93abab83c31e7a73283
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-02-03 12:51:45 +0100

    Lesson 7_2

---
+++ Java_projects/Lesson_7_2/src/com/gmail/tiutiuniryna/Main.java
@@ -0,0 +1,26 @@
+package com.gmail.tiutiuniryna;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Draw a rectangle of stars");
+		System.out.println("Enter Height");
+		int height = sc.nextInt();
+		System.out.println("Enter Widht");
+		int widht = sc.nextInt();
+		printRectangle(height, widht);
+	}
+
+	public static void printRectangle(int height, int widht) {
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < widht; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println("   ");
+		}
+	}
+
+}
