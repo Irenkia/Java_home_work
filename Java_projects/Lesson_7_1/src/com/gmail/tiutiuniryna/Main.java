6ce0f1a7bcad4cf68e5ede0bd496e08ae48a19f3
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-02-03 12:38:30 +0100

    Lesson 7_1

---
+++ Java_projects/Lesson_7_1/src/com/gmail/tiutiuniryna/Main.java
@@ -0,0 +1,33 @@
+package com.gmail.tiutiuniryna;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Random rn = new Random();
+		int[] arr = new int[15];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rn.nextInt(1, 20);
+		}
+		System.out.println("Our array");
+		System.out.println(Arrays.toString(arr));
+		int max = findMaxNumber(arr);
+		
+		System.out.println("Max number in our array");
+		System.out.println(max);
+
+	}
+
+	public static int findMaxNumber(int[] arr) {
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+
+}
