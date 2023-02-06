cb22306c2ed68fe161849df560c7a236b7a553a0
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-02-06 17:27:06 +0100

    Lesson 8_3

--- Java_projects/Lesson_8_2/src/com/gmail/tiutiuniryna/Main.java
+++ Java_projects/Lesson_8_2/src/com/gmail/tiutiuniryna/Main.java
@@ -3,7 +3,6 @@ package com.gmail.tiutiuniryna;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
